0299e0f1aef08f89c700dfa78ddb5184a0d08719
Raja Shankar Kolluru <raja.shankar@meratransport.com>
2019-09-19 07:37:16 +0530

    Test cases for scheduler

--- chenile-scheduler/src/test/java/org/chenile/scheduler/test/TestChenileScheduler.java
+++ chenile-scheduler/src/test/java/org/chenile/scheduler/test/TestChenileScheduler.java
@@ -1,16 +1,12 @@
 package org.chenile.scheduler.test;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import org.chenile.core.context.ChenileExchange;
-import org.chenile.core.entrypoint.ChenileEntryPoint;
-import org.chenile.core.model.ChenileConfiguration;
-import org.chenile.core.model.ChenileServiceDefinition;
-import org.chenile.core.model.OperationDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,39 +17,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("unittest")
 public class TestChenileScheduler {
 
-    @Autowired ChenileEntryPoint chenileEntryPoint;
-    @Autowired ChenileConfiguration chenileConfiguration;
-    public static boolean fooServiceInvoked = false;
-    private CountDownLatch latch = new CountDownLatch(1);
+    public static CountDownLatch latch = new CountDownLatch(1);
       
     @Test public void testIt() throws InterruptedException { 	
-		ChenileExchange exchange = makeExchange("fooService","schedule");
-//		exchange.setBody(fooM);
-		chenileEntryPoint.execute(exchange);
-		//Runs at `5` second interval.
-		//Wait for few seconds before shutting down the test.
-		latch.await(10, TimeUnit.SECONDS);
+		assertTrue("Failed: timed out in 10 seconds without executing the test",
+				latch.await(10, TimeUnit.SECONDS));
     }
-    
-    private ChenileServiceDefinition findService(String serviceName) {
-		return chenileConfiguration.getServices().get(serviceName);
-	}
-	
-	private OperationDefinition findOperationInService(ChenileServiceDefinition serviceDefinition, String opName) {
-		for (OperationDefinition od: serviceDefinition.getOperations()) {
-			if (od.getName().equals(opName)){
-				return od;
-			}
-		}
-		return null;
-	}
-	
-	private ChenileExchange makeExchange(String serviceName, String opName) {
-		ChenileServiceDefinition serviceDefinition = findService(serviceName);
-		OperationDefinition operationDefinition = findOperationInService(serviceDefinition, opName);
-		ChenileExchange exchange = new ChenileExchange();
-		exchange.setServiceDefinition(serviceDefinition);
-		exchange.setOperationDefinition(operationDefinition);
-		return exchange;
-	}
 }
--- chenile-scheduler/src/test/java/org/chenile/scheduler/test/service/FooService.java
+++ chenile-scheduler/src/test/java/org/chenile/scheduler/test/service/FooService.java
@@ -2,8 +2,11 @@ package org.chenile.scheduler.test.service;
 
 import java.util.Date;
 
+import org.chenile.scheduler.test.TestChenileScheduler;
+
 public class FooService {
 	public void schedule() {
 		System.out.println("I am executed at: " + new Date());
+		TestChenileScheduler.latch.countDown();
 	}
 }
