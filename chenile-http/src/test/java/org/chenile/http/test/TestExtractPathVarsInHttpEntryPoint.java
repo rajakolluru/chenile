53e13192b5cf8664267292b80d88ab34615559b9
Raja Shankar Kolluru <rajashankarkolluru@Rajas-MacBook-Pro.local>
2024-03-24 09:31:51 +0530

    fixed path vars issue

--- chenile-http/src/test/java/org/chenile/http/test/TestExtractPathVarsInHttpEntryPoint.java
+++ chenile-http/src/test/java/org/chenile/http/test/TestExtractPathVarsInHttpEntryPoint.java
@@ -3,28 +3,38 @@ package org.chenile.http.test;
 import java.util.Map;
 
 import org.chenile.http.handler.HttpEntryPoint;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("unittest")
 public class TestExtractPathVarsInHttpEntryPoint {
 	
-	public static void main(String[] args) {
-		func("/xxx/{id}", "/xxx/abc");
-		func("/xxx/{id}/{name}", "/xxx/abc/def");
-		func("/xxx/{id}/{name}/","/xxx/abc/def/");
-		func("/xxx/{id}/{name}/","/xxx/abc/def");
-	}
-	public static void func(String s1, String s2) {
-		System.out.println("Trying for " + s1 + " and " + s2);
-		// extractPathVariables(s1,s2);
-	}
-	@Test void testPathVars1() {
+	@Test public void testPathVars1() {
 		Map<String, Object> m = HttpEntryPoint.extractPathVariables("/xxx/{id}", "/xxx/abc");
 		assertMapContains(m,"id", "abc");
 	}
-	private void assertMapContains(Map<String, Object> m, String string, String string2) {
-		
-		
+	
+	@Test public void testPathVars2() {
+		Map<String, Object> m = HttpEntryPoint.extractPathVariables("/xxx/{id}/{name}", "/xxx/abc/def");
+		assertMapContains(m,"id", "abc");
+		assertMapContains(m,"name", "def");
+	}
+	
+	@Test public void testPathVars3() {
+		Map<String, Object> m = HttpEntryPoint.extractPathVariables("/xxx/{id}/{name}/", "/xxx/abc/def/");
+		assertMapContains(m,"id", "abc");
+		assertMapContains(m,"name", "def");
+	}
+	
+	@Test public void testPathVars4() {
+		Map<String, Object> m = HttpEntryPoint.extractPathVariables("/xxx/{id}/{name}/", "/xxx/abc/def");
+		assertMapContains(m,"id", "abc");
+		assertMapContains(m,"name", "def");
+	}
+	private void assertMapContains(Map<String, Object> m, String key, String value) {
+		Assert.assertNotNull(m);
+		Assert.assertTrue(m.containsKey(key));
+		Assert.assertEquals(value, m.get(key));
 	}
 }
