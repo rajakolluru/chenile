0cfeb7154f35feea3445281468e0008647d7a96d
Raja Shankar Kolluru <raja@thekollurus.com>
2024-04-06 09:14:17 +0530

    changed comment

--- chenile-core/src/main/java/org/chenile/core/interceptors/ValidateCopyHeaders.java
+++ chenile-core/src/main/java/org/chenile/core/interceptors/ValidateCopyHeaders.java
@@ -1,19 +1,22 @@
 package org.chenile.core.interceptors;
 
 import org.chenile.base.exception.ErrorNumException;
-import org.chenile.base.response.ResponseMessage;
-import org.chenile.base.response.WarningAware;
 import org.chenile.core.context.ChenileExchange;
 import org.chenile.core.context.ContextContainer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-
-import java.util.List;
-import java.util.Locale;
 
 /**
  *
  * @author Raja Shankar Kolluru
+ * As per HTTP convention, all custom headers start with "x-"
+ * This interceptor copies all  headers that start with "x-" into the context.
+ * The context can then be used to read the values by subsequent interceptors and other classes.
+ * If headers start with x-p- then they are considered protected. This means that
+ * they cannot be passed from the outside by the requester. Attempts to pass
+ * them will result in a security exception. This is because we consider such 
+ * requests as attempts to hack the system. These internal headers will need to be pushed into the 
+ * system by subsequent interceptors. 
+ * Please see chenile headers for more details.
  *
  */
 public class ValidateCopyHeaders extends BaseChenileInterceptor{
